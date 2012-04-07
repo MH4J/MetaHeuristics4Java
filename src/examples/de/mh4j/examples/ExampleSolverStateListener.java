9810762ae92f7b794eda35634660d4ef826683aa
Friedrich Große <friedrich.grosse@gmail.de>
2012-04-07 17:15:50 +0200

    Added an example implementation of a solver state listener that prints state changes to the system console.

---
+++ src/examples/de/mh4j/examples/ExampleSolverStateListener.java
@@ -0,0 +1,28 @@
+package de.mh4j.examples;
+
+import de.mh4j.examples.solver.SolverStateAdapter;
+import de.mh4j.solver.Solution;
+import de.mh4j.solver.Solver;
+
+public class ExampleSolverStateListener<GenericSolutionType extends Solution> extends
+        SolverStateAdapter<GenericSolutionType> {
+
+    GenericSolutionType lastSolution = null;
+
+    @Override
+    public void solverHasStepped(Solver<GenericSolutionType> solver) {
+        GenericSolutionType currentSolution = solver.getCurrentSolution();
+        if (currentSolution.equals(lastSolution) == false) {
+            System.out.print("New Solution: " + currentSolution);
+            System.out.println(" (Costs " + currentSolution.getCosts() + ")");
+            lastSolution = currentSolution;
+        }
+    }
+
+    @Override
+    public void solverHasFinished(Solver<GenericSolutionType> solver) {
+        System.out.println("Terminated after " + solver.getNumberOfSteps() + " steps");
+        System.out.println(solver.getCurrentSolution());
+    }
+
+}
