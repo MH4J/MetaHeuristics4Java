8102896e654b6bfbc7722afec29b3d2a0f23e5af
Yannik Stein <yannik.stein@fu-berlin.de>
2012-05-16 10:55:05 +0200

    fix issue #10
    delete too restrictive test 'LocalSearchSorter#createRandomNeighbor'

--- MetaHeuristics4Java-Examples/src/test/java/de/mh4j/examples/solver/LocalSearchSorterTest.java
+++ MetaHeuristics4Java-Examples/src/test/java/de/mh4j/examples/solver/LocalSearchSorterTest.java
@@ -2,8 +2,6 @@ package de.mh4j.examples.solver;
 
 import org.testng.annotations.Test;
 
-import de.mh4j.examples.Sorting;
-
 public class LocalSearchSorterTest {
     @Test
     public void testCreateInitialSolution() {
@@ -14,17 +12,5 @@ public class LocalSearchSorterTest {
          * if the solver could create any solution without throwing an exception
          * this test succeeded. (Sorting class handles its validity by itself)
          */
-    }
-
-    @Test
-    public void testCreateRandomNeighbor() {
-        LocalSearchSorter sorter = new LocalSearchSorter(10);
-        // invoke step to initialize the solver
-        sorter.step();
-
-        Sorting initialSorting = sorter.getCurrentSolution();
-        Sorting neighbor = sorter.createRandomNeighbor();
-
-        assert initialSorting.equals(neighbor) == false;
     }
 }
