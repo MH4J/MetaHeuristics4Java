41ef376caa53d67793dc7a4bad8c7ead042bbf15
Yannik Stein <yannik.stein@fu-berlin.de>
2012-05-27 22:57:21 +0200

    add license and license-header

--- MetaHeuristics4Java/src/main/java/de/mh4j/solver/simulatedAnnealing/AbstractCoolingScheme.java
+++ MetaHeuristics4Java/src/main/java/de/mh4j/solver/simulatedAnnealing/AbstractCoolingScheme.java
@@ -1,3 +1,23 @@
+/*
+ * Copyright 2012   Friedrich Große, Paul Seiferth,
+ *                  Sebastian Starroske, Yannik Stein
+ *
+ * This file is part of MetaHeuristics4Java.
+ *
+ * MetaHeuristics4Java is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MetaHeuristics4Java is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MetaHeuristics4Java. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package de.mh4j.solver.simulatedAnnealing;
 
 import org.slf4j.Logger;
