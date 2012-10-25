906a728f196160c5af1615f5a434dda6790eedeb
Friedrich Große <friedrich.grosse@gmail.de>
2012-10-25 23:50:24 +0200

    MH4J Util class does now use the RNG Generator

--- MetaHeuristics4Java/src/main/java/de/mh4j/util/Util.java
+++ MetaHeuristics4Java/src/main/java/de/mh4j/util/Util.java
@@ -27,14 +27,16 @@ import java.util.Random;
 
 public abstract class Util {
 
-	public static <T> List<T> reverse(List<T> originalList) {
-		List<T> reversedList = new ArrayList<T>(originalList);
-		Collections.reverse(reversedList);
-		return reversedList;		
-	}
-	
-	public static <T> T getRandomEntryFrom(List<T> list, Random randomizer) {
-		int randomIndex = randomizer.nextInt(list.size());
-		return list.get(randomIndex);
-	}
+    private final static Random randomizer = RNGGenerator.createRandomNumberGenerator();
+
+    public static <T> List<T> reverse(List<T> originalList) {
+        List<T> reversedList = new ArrayList<T>(originalList);
+        Collections.reverse(reversedList);
+        return reversedList;
+    }
+
+    public static <T> T getRandomEntryFrom(List<T> list) {
+        int randomIndex = randomizer.nextInt(list.size());
+        return list.get(randomIndex);
+    }
 }
