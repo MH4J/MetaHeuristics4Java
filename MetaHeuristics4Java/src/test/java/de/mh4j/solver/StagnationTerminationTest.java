47446b2a8ca6cb4dcc7be6740e288007ffbd5cf5
Friedrich Große <friedrich.grosse@gmail.de>
2012-11-10 13:28:57 +0100

    Fixed the failing StagnationTerminationTest (hopefully)

--- MetaHeuristics4Java/src/test/java/de/mh4j/solver/StagnationTerminationTest.java
+++ MetaHeuristics4Java/src/test/java/de/mh4j/solver/StagnationTerminationTest.java
@@ -10,44 +10,23 @@ import de.mh4j.solver.termination.TerminationCondition;
 
 public class StagnationTerminationTest {
 
-	@Test
-	@SuppressWarnings("unchecked")
-	public void testShouldTerminate() {
-		int maxNrOfStagnatingSteps = 50;
-		Solver<Solution<?>> solver = mock(Solver.class);
+    @Test
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+    public void testShouldTerminate() {
+        int maxNrOfStagnatingSteps = 50;
+        Solver solver = mock(Solver.class);
+        when(solver.getCurrentSolution()).thenReturn(mock(Solution.class));
+        TerminationCondition terminator = new StagnationTermination(solver, maxNrOfStagnatingSteps);
 
-		when(solver.getCurrentSolution()).thenReturn(createSolution());
-		TerminationCondition terminator = new StagnationTermination(solver,
-				maxNrOfStagnatingSteps);
+        assert terminator.shouldTerminate() == false;
 
-		assert terminator.shouldTerminate() == false;
+        for (int i = 0; i < maxNrOfStagnatingSteps - 1; i++) {
+            assert terminator.shouldTerminate() == false;
+        }
 
-		for (int i = 0; i < maxNrOfStagnatingSteps - 1; i++) {
-			assert terminator.shouldTerminate() == false;
-		}
+        assert terminator.shouldTerminate() == true : "Terminator should terminate now because we asked it "
+                + maxNrOfStagnatingSteps
+                + " if we should terminate without any improvement in the associated solver";
+    }
 
-		assert terminator.shouldTerminate() == true : "Terminator should terminate now because we asked it "
-				+ maxNrOfStagnatingSteps
-				+ " if we should terminate without any improvement in the associated solver";
-	}
-
-	private Solution<Solution<?>> createSolution() {
-		return new Solution<Solution<?>>() {
-
-			@Override
-			public boolean isBetterThan(Solution<?> otherSolution) {
-				return false;
-			}
-
-			@Override
-			public int getCosts() {
-				return 0;
-			}
-
-			@Override
-			public boolean equals(Object obj) {
-				return true;
-			}
-		};
-	}
 }
