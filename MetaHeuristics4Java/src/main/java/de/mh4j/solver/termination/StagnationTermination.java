4b7619f26fde8ea9ad7cc351f9ee65c34231a064
Sebastian Starroske <sebastian.starroske@googlemail.com>
2012-11-10 10:38:22 +0100

    Fixed StagnationTermination.shouldFinish()
    The shouldFinish() method used the "==" operator to compare if the
    currentSolution has changed. Since many neighbors are created as
    copies of the origin solution this will never be true.
    
    Now the method uses the equals() method instead.

--- MetaHeuristics4Java/src/main/java/de/mh4j/solver/termination/StagnationTermination.java
+++ MetaHeuristics4Java/src/main/java/de/mh4j/solver/termination/StagnationTermination.java
@@ -5,34 +5,35 @@ import de.mh4j.solver.Solver;
 
 public class StagnationTermination implements TerminationCondition {
 
-    private final Solver<? extends Solution<?>> solver;
-    private final int maxNrOfStagnatingSteps;
+	private final Solver<? extends Solution<?>> solver;
+	private final int maxNrOfStagnatingSteps;
 
-    private int nrOfStagnatingSteps;
-    private Solution<?> lastSolution;
+	private int nrOfStagnatingSteps;
+	private Solution<?> lastSolution;
 
-    public StagnationTermination(Solver<? extends Solution<?>> solver, int maxNrOfStagnatingSteps) {
-        this.solver = solver;
-        this.maxNrOfStagnatingSteps = maxNrOfStagnatingSteps;
-        this.nrOfStagnatingSteps = 0;
-    }
+	public StagnationTermination(Solver<? extends Solution<?>> solver,
+			int maxNrOfStagnatingSteps) {
+		this.solver = solver;
+		this.maxNrOfStagnatingSteps = maxNrOfStagnatingSteps;
+		this.nrOfStagnatingSteps = 0;
+	}
 
-    @Override
-    public boolean shouldTerminate() {
-        Solution<?> currentSolution = solver.getCurrentSolution();
-        if (currentSolution == lastSolution) {
-            nrOfStagnatingSteps++;
-        }
-        else {
-            nrOfStagnatingSteps = 0;
-            lastSolution = currentSolution;
-        }
+	@Override
+	public boolean shouldTerminate() {
+		Solution<?> currentSolution = solver.getCurrentSolution();
+		if (currentSolution.equals(lastSolution)) {
+			nrOfStagnatingSteps++;
+		} else {
+			nrOfStagnatingSteps = 0;
+			lastSolution = currentSolution;
+		}
 
-        return nrOfStagnatingSteps >= maxNrOfStagnatingSteps;
-    }
+		return nrOfStagnatingSteps >= maxNrOfStagnatingSteps;
+	}
 
-    @Override
-    public String toString() {
-        return "stagnation termination condition (" + maxNrOfStagnatingSteps + " non-improving steps)";
-    }
+	@Override
+	public String toString() {
+		return "stagnation termination condition (" + maxNrOfStagnatingSteps
+				+ " non-improving steps)";
+	}
 }
--- MetaHeuristics4Java/src/test/java/de/mh4j/solver/StagnationTerminationTest.java
+++ MetaHeuristics4Java/src/test/java/de/mh4j/solver/StagnationTerminationTest.java
@@ -1,6 +1,7 @@
 package de.mh4j.solver;
 
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import org.testng.annotations.Test;
 
@@ -9,20 +10,44 @@ import de.mh4j.solver.termination.TerminationCondition;
 
 public class StagnationTerminationTest {
 
-    @Test
-    @SuppressWarnings("unchecked")
-    public void testShouldTerminate() {
-        int maxNrOfStagnatingSteps = 50;
-        Solver<Solution<?>> solver = mock(Solver.class);
-        TerminationCondition terminator = new StagnationTermination(solver, maxNrOfStagnatingSteps);
+	@Test
+	@SuppressWarnings("unchecked")
+	public void testShouldTerminate() {
+		int maxNrOfStagnatingSteps = 50;
+		Solver<Solution<?>> solver = mock(Solver.class);
 
-        assert terminator.shouldTerminate() == false;
+		when(solver.getCurrentSolution()).thenReturn(createSolution());
+		TerminationCondition terminator = new StagnationTermination(solver,
+				maxNrOfStagnatingSteps);
 
-        for (int i = 1; i < maxNrOfStagnatingSteps - 1; i++) {
-            assert terminator.shouldTerminate() == false;
-        }
+		assert terminator.shouldTerminate() == false;
 
-        assert terminator.shouldTerminate() == true : "Terminator should terminate now because we asked it "
-                + maxNrOfStagnatingSteps + " if we should terminate without any improvement in the associated solver";
-    }
+		for (int i = 1; i < maxNrOfStagnatingSteps - 1; i++) {
+			assert terminator.shouldTerminate() == false;
+		}
+
+		assert terminator.shouldTerminate() == true : "Terminator should terminate now because we asked it "
+				+ maxNrOfStagnatingSteps
+				+ " if we should terminate without any improvement in the associated solver";
+	}
+
+	private Solution<Solution<?>> createSolution() {
+		return new Solution<Solution<?>>() {
+
+			@Override
+			public boolean isBetterThan(Solution<?> otherSolution) {
+				return false;
+			}
+
+			@Override
+			public int getCosts() {
+				return 0;
+			}
+
+			@Override
+			public boolean equals(Object obj) {
+				return true;
+			}
+		};
+	}
 }
